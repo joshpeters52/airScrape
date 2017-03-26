17521b0cfe85a00e4d811c2dab7ccf762a690852
Josh <joshpeters52@gmail.com>
2017-03-25 20:33:44 -0400

    added wget

--- reader.py
+++ reader.py
@@ -15,11 +15,13 @@ with open('tweets.json', 'r') as f:
 		name = tweet['user']['name']
 		screenName = tweet['user']['screen_name']
 		try:
+			count+=1
 			mediaUrl = tweet['extended_entities']['media'][0]['media_url']
 			file.write(name + ',' + screenName + ',' + mediaUrl + '\n')
-			filename = wget.download(mediaUrl)
+			filename = wget.download(mediaUrl, out = 'data/images/' + str(count)+'.jpg')
 		except:
 			print('error on ' + str(count))
 			mediaUrl = ''
+			count-=1
 			pass
 
