f1508716cda1d78475fbc67587a4485ddfaf967e
rjdean123@gmail.com <rjdean123@gmail.com>
2017-03-26 01:06:42 -0400

    update2

--- streamListener.py
+++ streamListener.py
@@ -65,10 +65,33 @@ class MyListener(StreamListener):
                 file.write(a+'-')
             file.write(',' + airline)
             file.write('\n')
+            self.sendEmail()
         except Exception as e:
             print(e)
             mediaUrl = ''
  
+    def sendEmail(self):
+        gmail_user = "airscraper.scrappy@gmail.com"
+        gmail_pwd = "badandboujee"
+        FROM = "airscraper.scrappy@gmail.com"
+        TO = "rjdean123@gmail.com"
+        message = "hello world"
+
+        msg = MIMEText("I found you another vulnerable flight! Check it out at http://localhost:5000")
+        msg['Subject'] = 'AIRSCRAPE | New Flight!'
+        me = 'rjdean123@gmail.com'
+        you = 'rjdean123@gmail.com'
+        msg['From'] = me
+        msg['To'] = you
+        
+        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server_ssl.ehlo() # optional, called by login()
+        server_ssl.login(gmail_user, gmail_pwd)  
+        # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
+        server_ssl.sendmail(FROM, TO, msg.as_string())
+        #server_ssl.quit()
+        server_ssl.close()
+
     def on_data(self, data):
         self.handle_data(data)
         return True
